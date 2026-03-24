82c4566fb5c233cdd4b7618d988f889272e667e0
Manish Rai <112763542+rai8896@users.noreply.github.com>
2026-03-24 19:34:32 +0530

    Updated Solution - GfG to GitHub

--- Difficulty: Medium/Directed Graph Cycle/directed-graph-cycle.cpp
+++ Difficulty: Medium/Directed Graph Cycle/directed-graph-cycle.cpp
@@ -1,34 +1,100 @@
 class Solution {
   public:
-  bool DetectCycle(int node,vector<int>adj[],vector<bool>&path,vector<bool>&visited)
-  {
+//   bool DetectCycle(int node,vector<int>adj[],vector<bool>&path,vector<bool>&visited)
+//   {
       
-      visited[node]=1;
-      path[node]=1;
-      for(int j=0;j<adj[node].size();j++)
-      {
-          if(path[adj[node][j]])
-          {
-              return 1;
-          }
-          if(visited[adj[node][j]] )
-          {
-              continue;
-          }
-          if(DetectCycle(adj[node][j],adj,path,visited))
-          {
-              return 1;
-          }
-      }
-      path[node]=0;
-      return 0;
+//       visited[node]=1;
+//       path[node]=1;
+//       for(int j=0;j<adj[node].size();j++)
+//       {
+//           if(path[adj[node][j]])
+//           {
+//               return 1;
+//           }
+//         if(visited[adj[node][j]] )
+//           {
+//               continue;
+//           }  
+//           if(DetectCycle(adj[node][j],adj,path,visited))
+//           {
+//               return 1;
+//           }
+//       }
+//       path[node]=0;
+//       return 0;
       
       
-  }
+//   }
     bool isCyclic(int V, vector<vector<int>> &edges) {
         // code here
         
         
+    //     vector<int>adj[V];
+    //     for(auto edge:edges)
+    //     {
+    //         int u=edge[0];
+    //         int v=edge[1];
+    //         adj[u].push_back(v);
+    //     }
+        
+        
+        
+        
+    // vector<bool>path(V,0);
+    // vector<bool>visited(V,0);
+    // for(int i=0;i<V;i++)
+    // {
+    //     if(!visited[i] && DetectCycle(i,adj,path,visited))
+    //     {
+    //         return 1;
+    //     }
+    // }
+    // return 0;
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
         vector<int>adj[V];
         for(auto edge:edges)
         {
@@ -37,19 +103,52 @@ class Solution {
             adj[u].push_back(v);
         }
         
-        
-        
-        
-    vector<bool>path(V,0);
-    vector<bool>visited(V,0);
+    
+    
+    
+    
+    
+    
+    
+    
+    vector<int>ans;
+    vector<int>InDeg(V,0);
+    // calculation of indegree
     for(int i=0;i<V;i++)
     {
-        if(!visited[i] && DetectCycle(i,adj,path,visited))
+        for(int j=0;j<adj[i].size();j++)
         {
-            return 1;
+            InDeg[adj[i][j]]++;
         }
     }
-    return 0;
+    
+    queue<int>q;
+    for(int i=0;i<V;i++)
+    {
+        if(InDeg[i]==0)
+        {
+            q.push(i);
+        }
+    }
+    
+    while(!q.empty())
+    {
+        int node=q.front();
+        q.pop();
+        ans.push_back(node);
+        for(int j=0;j<adj[node].size();j++)
+        {
+            InDeg[adj[node][j]]--;
+            if( InDeg[adj[node][j]]==0)
+            {
+                q.push(adj[node][j]);
+            }
+        }
+    }
+    int count=ans.size();
+    return count!=V;
+    
+    
     
    }
 };
