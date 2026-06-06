bfa703fc11907d5358dcee21a6a177d95794ffd1
Manish Rai <112763542+rai8896@users.noreply.github.com>
2026-06-06 07:45:16 +0530

    Updated Solution - GfG to GitHub

--- Difficulty: Medium/Directed Graph Cycle/directed-graph-cycle.cpp
+++ Difficulty: Medium/Directed Graph Cycle/directed-graph-cycle.cpp
@@ -1,154 +1,56 @@
 class Solution {
   public:
-//   bool DetectCycle(int node,vector<int>adj[],vector<bool>&path,vector<bool>&visited)
-//   {
-      
-//       visited[node]=1;
-//       path[node]=1;
-//       for(int j=0;j<adj[node].size();j++)
-//       {
-//           if(path[adj[node][j]])
-//           {
-//               return 1;
-//           }
-//         if(visited[adj[node][j]] )
-//           {
-//               continue;
-//           }  
-//           if(DetectCycle(adj[node][j],adj,path,visited))
-//           {
-//               return 1;
-//           }
-//       }
-//       path[node]=0;
-//       return 0;
+  bool dfs(int node,vector<int>adj[],int pathVis[],int vis[])
+  {
+      vis[node]=1;
+      pathVis[node]=1;
       
       
-//   }
-    bool isCyclic(int V, vector<vector<int>> &edges) {
+      //traverse for adjacent node
+      for(auto it:adj[node])
+      {
+          // if path is not visited
+          if(!vis[it])
+          {
+              if(dfs(it,adj,pathVis,vis)==true) return true;
+              
+              
+          }
+              // if niode is previsioly visited but it has to be visited on the same path
+              else if(pathVis[it])
+              {
+                  return true;
+              }
+          
+      }
+      pathVis[node]=0;
+      return false;
+  }
+    bool isCyclic(int V, vector<vector<int>> &edge ) {
         // code here
         
         
-    //     vector<int>adj[V];
-    //     for(auto edge:edges)
-    //     {
-    //         int u=edge[0];
-    //         int v=edge[1];
-    //         adj[u].push_back(v);
-    //     }
-        
-        
-        
-        
-    // vector<bool>path(V,0);
-    // vector<bool>visited(V,0);
-    // for(int i=0;i<V;i++)
-    // {
-    //     if(!visited[i] && DetectCycle(i,adj,path,visited))
-    //     {
-    //         return 1;
-    //     }
-    // }
-    // return 0;
-    
-    
-    
-    
-    
-    
-    
-    
-    
-    
-    
-    
-    
-    
-    
-    
-    
-    
-    
-    
-    
-    
-    
-    
-    
-    
-    
-    
-    
-    
-    
-    
-    
-    
-    
-    
-    
-    
-    
-    
-    
-    
-    
-    
-    
+        //adjancy list bnao
         vector<int>adj[V];
-        for(auto edge:edges)
+        
+        for(auto & it:edge)
         {
-            int u=edge[0];
-            int v=edge[1];
+            int u=it[0];
+            int v=it[1];
             adj[u].push_back(v);
+        
         }
         
-    
-    
-    
-    
-    
-    
-    
-    
-    vector<int>ans;
-    vector<int>InDeg(V,0);
-    // calculation of indegree
-    for(int i=0;i<V;i++)
-    {
-        for(int j=0;j<adj[i].size();j++)
+        
+        int vis[V]={0};
+        int pathVis[V]={0};
+        
+        for(int i=0;i<V;i++)
         {
-            InDeg[adj[i][j]]++;
+           if( dfs(i,adj,pathVis,vis) ==true)  return true;
         }
-    }
-    
-    queue<int>q;
-    for(int i=0;i<V;i++)
-    {
-        if(InDeg[i]==0)
-        {
-            q.push(i);
+        return false;
+        
+            
         }
-    }
-    
-    while(!q.empty())
-    {
-        int node=q.front();
-        q.pop();
-        ans.push_back(node);
-        for(int j=0;j<adj[node].size();j++)
-        {
-            InDeg[adj[node][j]]--;
-            if( InDeg[adj[node][j]]==0)
-            {
-                q.push(adj[node][j]);
-            }
-        }
-    }
-    int count=ans.size();
-    return count!=V;
-    
-    
-    
-   }
 };
