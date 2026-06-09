75500bd61f75b7d811b8c92e7f499df4947ad811
Manish Rai <112763542+rai8896@users.noreply.github.com>
2026-06-09 08:34:10 +0530

    Added Solution - GfG to GitHub

---
+++ Difficulty: Medium/Bellman-Ford/bellman-ford.cpp
@@ -0,0 +1,42 @@
+// User function Template for C++
+
+class Solution {
+  public:
+    vector<int> bellmanFord(int V, vector<vector<int>>& edges, int src) {
+        // Code here
+        vector<int>dist(V,1e8);
+        dist[src]=0;
+        for(int count=1;count<=V-1;count++)
+        {
+            for(auto edge:edges)
+            {
+                 int u=edge[0];
+                 int v=edge[1];
+                 int w=edge[2];
+                 
+                 
+                 if(dist[u]!=1e8 && dist[u]+w<dist[v])
+                 {
+                     dist[v]=dist[u]+w;
+                     
+                 }
+            }
+        }
+        
+        
+        for(auto edge:edges)
+            {
+                 int u=edge[0];
+                 int v=edge[1];
+                 int w=edge[2];
+                 
+                 
+                 if(dist[u]!=1e8 && dist[u]+w<dist[v])
+                 {
+                     return {-1};
+                     
+                 }
+            }
+             return dist;
+    }
+};
